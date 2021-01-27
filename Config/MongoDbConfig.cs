47ba1e4f61c651ada65fefc8f60814e17183f9e5
Alaa <alaa.omer2009@gmail.com>
2021-01-27 14:23:05 +0200

    connesction

--- Config/MongoDbConfig.cs
+++ Config/MongoDbConfig.cs
@@ -18,10 +18,6 @@ namespace ErrorMessagesAPI.Config
 
             }
         }
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
         public string ConnectionString
         {
             get
