4716147e93c809232a22fefb6d3b844b63be1748
Alaa <alaa.omer2009@gmail.com>
2021-01-29 11:59:07 +0200

    deploy to k8s

--- Config/MongoDbConfig.cs
+++ Config/MongoDbConfig.cs
@@ -9,22 +9,54 @@ namespace ErrorMessagesAPI.Config
 {
     public class MongoDBConfig
     {
-        public string Database
+        public string Database { get
+            {
+                return Environment.GetEnvironmentVariable("database");
+            }
+
+                
+                }
+        public string Host
         {
             get
             {
-
-                return Environment.GetEnvironmentVariable("Mongo_DB");
-
+                return Environment.GetEnvironmentVariable("Host");
             }
+
+
+        }
+        public int Port { get
+            {
+                return 27017;
+            }
+                }
+        public string User
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("username");
+            }
+
+
+        }
+        public string Password
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("password");
+            }
+
+
         }
         public string ConnectionString
         {
             get
             {
-                
-               return  Environment.GetEnvironmentVariable("MongoDB__URL");
+                if (string.IsNullOrEmpty(User) || string.IsNullOrEmpty(Password))
+                    return $@"mongodb://{Host}:{Port}";
 
+                return $@"mongodb://{User}:{Password}@{Host}:{Port}/{Database}";
+                
             }
         }
     }
---
+++ initmongo.js
@@ -0,0 +1,22 @@
+﻿use admin
+db.auth('root', 'example')
+
+db = db.getSiblingDB('messagesdb')
+db.createCollection("messages", {})
+//db.createUser({
+//    user: 'admin',
+//    pwd: 'admin',
+//    roles: [
+//        {
+//            role: 'dbOwner',
+//            db: 'test'
+//        }
+//    ]
+//});
+
+db.test.insert(
+    [
+        { text: "tes1", createdAt: ISODate("2017-10-13T10:53:53Z"), updatedAt: ISODate("2017-10-13T10:53:53Z") },
+        { text: "test2", createdAt: ISODate("2017-10-13T10:53:53Z"), updatedAt: ISODate("2017-10-13T10:53:53Z") },
+    ]
+);
